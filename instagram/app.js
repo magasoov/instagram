24358d908d012eba1622c3755de2ba7d661bd7dd
Muhammad <148522477+magasoov@users.noreply.github.com>
2024-03-05 13:06:59 +0300

    Add files via upload

---
+++ instagram/app.js
@@ -0,0 +1,20 @@
+function toggleHiddenDiv() {
+    var hiddenDiv = document.getElementById("hiddenDiv");
+
+    
+    var element = document.querySelector(".navlist");
+    var logo = document.querySelector(".logoDiv");
+    logo.style.display = "none";
+    element.style.display = "none";
+
+    if (hiddenDiv.style.display === "none") {
+      hiddenDiv.style.display = "block";
+    } else {
+      hiddenDiv.style.display = "none";
+    }
+  }
+function abc () {
+
+
+      
+}
